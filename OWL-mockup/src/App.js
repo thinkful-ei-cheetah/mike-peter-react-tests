df546c4e5635a15ca9b3ed3a2b5e1b75e1be8106
Mike Jung <kpnba90@gmail.com>
2019-04-16 17:13:44 -0400

    css added

--- OWL-mockup/src/App.js
+++ OWL-mockup/src/App.js
@@ -1,25 +1,17 @@
 import React from 'react';
-
 import PList from './participant-comp/participantList';
 import Stage from './stage-comp/stage';
-
-
 
 function App(props) {
 
 const pList = <PList people={props.store.participants} />
 const stage = <Stage actors={props.store.participants}/>
 
-
-
-
   return (
     <main className='App'>
       <header className="App-header">
         <h1>OWL markup</h1>
-       
-
-    </header>
+      </header>
       {pList} 
       {stage}
     </main>
