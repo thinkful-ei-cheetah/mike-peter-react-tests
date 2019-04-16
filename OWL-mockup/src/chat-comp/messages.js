b6d0ad0029c0e5a52267488b2105c2d04a76b7fb
Peter Pae <paendabear@gmail.com>
2019-04-16 17:23:03 -0400

    change files and directories

--- OWL-mockup/src/chat-comp/messages.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-
-function Message(props){
-    
-    return  <div>iam a message</div> 
-
-}
-export default Message
--- OWL-mockup/src/chat-comp/reactions.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-
-function Reaction(props){
-    
-    return  <div>iam a reaction </div> 
-
-}
-export default Reaction
